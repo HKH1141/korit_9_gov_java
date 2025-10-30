f0ff01d4e1dcce39752a70081835b19e4603a906
HKH1141 <cdm6585@gmail.com>
2025-10-30 17:23:46 +0900

    25-10-30 수업

--- src/main/java/com/korit/study/ch06/연습문제1.java
+++ src/main/java/com/korit/study/ch06/연습문제1.java
@@ -20,12 +20,12 @@ public class 연습문제1 {
         return number * number;
     }
 
-    public static String isNowTime(int hours, int minutes) {
-        Date now = new Date();
-        int nowHours = now.getHours();
-        int nowMinutes = now.getMinutes();
-
-        return hours == nowHours && minutes == nowMinutes ? "현재 시간입니다." : "현재 시간이 아닙니다.";
-    }
+//    public static String isNowTime(int hours, int minutes) {
+//        Date now = new Date();
+//        int nowHours = now.getHours();
+//        int nowMinutes = now.getMinutes();
+//
+//        return hours == nowHours && minutes == nowMinutes ? "현재 시간입니다." : "현재 시간이 아닙니다.";
+//    }
 
 }
